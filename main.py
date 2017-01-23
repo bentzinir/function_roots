178a8b1cb2c14c5b4f19ccccbfba6b5eae4914a3
galdl <galdl20@gmail.com>
2017-01-23 14:58:36 +0200

    first commit for the linear functions branch

--- main.py
+++ main.py
@@ -3,71 +3,76 @@ import numpy as np
 from root_finder import ROOT_FINDER
 import matplotlib.pyplot as plt
 import time
-from scipy.optimize import fsolve
+#from scipy.optimize import fsolve
 
 # TYPE='POLYNOMIAL'
-TYPE = 'TRIGONOMETRIC'
-
-
+# TYPE = 'TRIGONOMETRIC'
+TYPE = 'LINEAR'
+#
 def el_madad(f, params, roots_p, x_range, n_samples=20):
     sampled_x = np.random.uniform(low=x_range[0], high=x_range[1], size=n_samples)
     madad = []
     for p, (r1, r2) in zip(params, roots_p):
         if TYPE == 'POLYNOMIAL':
-            num = np.mean([abs(f([r1,0], p)), abs(f([r2,0], p))])
+            num = np.mean([abs(f(r1, p)), abs(f(r2, p))])
             den = []
             for x_sample in sampled_x:
-                den.append(f([x_sample,0], p))
+                den.append(f(x_sample, p))
 
-            madad.append(num / np.asarray(den)[:,0].max()-np.asarray(den)[:,0].min())
+            madad.append(num / (max(den) - min(den)))
 
         elif TYPE == 'TRIGONOMETRIC':
             num = f(np.asarray([r1, r2]), p)[0]
             madad.append(abs(num/p[0]))
 
-    return (np.asarray(madad)).mean()
 
+    # return (np.asarray(madad)).mean()
+    return 1
 
 def f(x, *args):
     params = args[0]
     if TYPE == 'POLYNOMIAL':
-        return np.asarray([np.dot(params, np.asarray([x[0]**2, x[0], 1])), 0])
+        return np.dot(params, np.asarray([x**2, x, 1]))
     elif TYPE == 'TRIGONOMETRIC':
         # return -params[0] + params[1]*np.sin(x[0]) + params[2]*np.sin(x[1]), 0
-        return np.asarray([-params[0] + params[1] * np.square(x[0]) + params[2] * np.abs(x[1]), 0])
-
+        return -params[0] + params[1] * np.square(x[0]) + params[2] * np.abs(x[1]), 0
+    elif TYPE == 'LINEAR':
+        return np.dot(params[0],x) - params[1]
 
 def fprime(x):
     return params[1]*np.cos(x[0]) + params[2]*np.cos(x[1])
 
 
 def get_root(params, x_dim):
-    a = params[0]
-    b = params[1]
-    c = params[2]
+    if TYPE == 'POLYNOMIAL':
+        a = params[0]
+        b = params[1]
+        c = params[2]
+        t = b**2 - 4 * a * c
+        if t >= 0:
+            disc = np.sqrt(t)
+            roots = np.asarray([(1/(2*a))*(-b - disc), (1/(2*a))*(-b + disc)])
+            return True, roots
+        else:
+            return False, []
+    elif TYPE == 'TRIGONOMETRIC':
+        sol = fsolve(f, x0=np.random.normal(size=x_dim), args=params)
+        if abs(np.asarray(f(sol, params))).sum() < 1e-4:
+            valid = True
+        else:
+            valid = False
+        return valid, sol
+    elif TYPE == 'LINEAR':
+        # sol = np.linalg.solve(params[0],params[1])
+        sol = params[1]/params[0]
+        if abs(np.asarray(f(sol, params))).sum() < 1e-4:
+            valid = True
+        else:
+            valid = False
+        return valid, sol
 
-    # if TYPE == 'POLYNOMIAL':
-        # t = b**2 - 4 * a * c
-        # if t >= 0:
-        #     disc = np.sqrt(t)
-        #     roots = np.asarray([(1/(2*a))*(-b - disc), (1/(2*a))*(-b + disc)])
-        #     return True, roots
-        # else:
-        #     return False, []
-    sol = fsolve(f, x0=np.random.normal(size=x_dim), args=params)
 
-    # elif TYPE == 'TRIGONOMETRIC':
-    #     sol = fsolve(f, x0=np.random.normal(size=x_dim), args=params)
-
-    if abs(np.asarray(f(sol, params))).sum() < 1e-4:
-        valid = True
-    else:
-        valid = False
-
-    return valid, sol
-
-
-def moving_average(a, n=100):
+def moving_average(a, n=100) :
     ret = np.cumsum(a, dtype=float)
     ret[n:] = ret[n:] - ret[:-n]
     return ret[n - 1:] / n
@@ -87,22 +92,28 @@ def draw_params(n_params, x_dim, batch_size):
             elif TYPE == 'TRIGONOMETRIC':
                 params = np.random.uniform(low=0, high=0.5, size=n_params)
 
+            elif TYPE == 'LINEAR':
+                params=[];
+                params.append(np.random.normal(np.zeros([x_dim,x_dim])))
+                params.append(np.random.normal(np.zeros(x_dim)))
+
             valid, roots = get_root(params, x_dim)
 
         params_batch.append(params)
         roots_batch.append(roots)
-    return np.asarray(params_batch), np.asarray(roots_batch)
+    # return np.asarray(params_batch), np.asarray(roots_batch)
+    return params_batch, roots_batch
 
 
 # 0. problem definition: calculate roots of a parameterized 2nd order polinom: ax^2 + bx + c
 
 # 1. define graph (Feed-forward network)
-n_params = 3
-n_roots = 2
-x_dim = 2
+x_dim = 1
+n_params = x_dim*(x_dim+1)
+n_roots = x_dim
 n_iters = 1000000
 net_size = [25, 25]
-batch_size = 32
+batch_size = 1 #32
 
 params = tf.placeholder("float", shape=(batch_size, n_params))
 roots = tf.placeholder("float", shape=(batch_size, n_roots))
@@ -138,6 +149,15 @@ plt.autoscale(enable=True, axis='both', tight=None)
 # 2. Train loop
 for i in xrange(n_iters):
     params_, roots_ = draw_params(n_params, x_dim, batch_size)
+    # a_inverse = np.linalg.inv(params_[0][0])
+    # a_flat = np.ndarray.flatten(params_[0][0])
+    # a_inverse_flat = np.ndarray.flatten(a_inverse)
+    # params_ = np.concatenate((a_inverse_flat,params_[0][1]))
+
+    a_inverse = 1/params_[0][0]
+    params_ = np.asarray([a_inverse,params_[0][1]])
+
+    params_.shape=(1,len(params_))
     run_vals = sess.run(fetches=[apply_grads, loss, roots_prediction, mean_abs_grad, mean_abs_w], feed_dict={params: params_, roots: roots_, dropout_keep: 1.})
     roots_p = run_vals[2]
 
@@ -146,8 +166,9 @@ for i in xrange(n_iters):
         madad_vec.append(madad)
         loss_vec.append(run_vals[1])
         iters_vec.append(i)
-        print 'Processed %d/%d iters. Loss: %.4f, madad: %.4f, abs_grad: %.2f, abs_w: %.2f, roots_mean: %.2f, roots_std: %.2f' %\
-              (i, n_iters, run_vals[1], madad, run_vals[3], run_vals[4], roots_.mean(), roots_.std())
+        print 'Processed %d/%d iters. Loss: %.4f, madad: %.4f, abs_grad: %.2f, abs_w: %.2f' %\
+              (i, n_iters, run_vals[1], madad, run_vals[3], run_vals[4])
+        plt.pause(0.001)
         l1.set_data(iters_vec, madad_vec)
         l2.set_data(iters_vec, loss_vec)
         axes.relim()
